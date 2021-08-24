57cf7f022614fb42e13300047c07f1f50c3a0495
padmini v <padmini.v@perfios.com>
2021-08-24 09:51:31 +0530

    Added:  Looping through days of gambling

--- src/com/bridgelabz/workshop/GamblingSimulator.java
+++ src/com/bridgelabz/workshop/GamblingSimulator.java
@@ -1,36 +1,54 @@
 package com.bridgelabz.workshop;
 
 public class GamblingSimulator {
-	public static int EVERYDAYSTAKE=100;
-	public static int BETPERGAME=1;
+	public static final int EVERYDAYSTAKE=100;                  //uc1
+	public static final int BETPERGAME=1;
 	
-	public void stakeStatus() {
+	int everyDayStake=EVERYDAYSTAKE;
+	int totalAmount=0;
+	
+	public void stakeStatus() 
+	{                                                        //uc2
 		int status=(int)(Math.random()*100);
 		if(status<=50)
 		{
 			System.out.println("Win");
-			EVERYDAYSTAKE++;
+			everyDayStake++;
 		}
 			
 		else
 		{
 			System.out.println("Loose");
-			EVERYDAYSTAKE--;
+			everyDayStake--;
 		}
 			
 	}
-	public void lossProfit() {
-		while(!(EVERYDAYSTAKE<=50 || EVERYDAYSTAKE>=150)){
+	public void gamblingDays()                                 //uc4
+	{
+		int days=20;
+		for(int index=1;index<=days;index++)
+		{
+			everyDayStake=100;
+			lossProfit();
+		}
+		System.out.println("total amount: "+ totalAmount);
+		
+		
+	}
+	public void lossProfit() {                                  //uc3
+		while(!(everyDayStake<=50 || everyDayStake>=150)){
 			stakeStatus();
 		}
+		totalAmount+=everyDayStake;
 		System.out.println("Gambling player has resigned for the day");
-		System.out.println("stake balance:"+ EVERYDAYSTAKE);
+		System.out.println("stake balance:"+ everyDayStake);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("***Welcome to the Gambling Simulator***");
 		GamblingSimulator object=new GamblingSimulator();
-		object.lossProfit();
+		object.gamblingDays();
+		
  
 	}
 
