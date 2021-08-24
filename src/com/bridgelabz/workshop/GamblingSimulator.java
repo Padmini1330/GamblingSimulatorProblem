366463fb7714c3fd67b8541c0b176ba76cb845bb
padmini v <padmini.v@perfios.com>
2021-08-24 09:13:04 +0530

    ADDED: Computed 50% of stake won or lost

--- src/com/bridgelabz/workshop/GamblingSimulator.java
+++ src/com/bridgelabz/workshop/GamblingSimulator.java
@@ -1,24 +1,36 @@
 package com.bridgelabz.workshop;
 
 public class GamblingSimulator {
-	public static final int EVERYDAYSTAKE=100;
-	public static final int BETPERGAME=1;
-	public void uc1() {
-		
-	}
-	public void uc2() {
+	public static int EVERYDAYSTAKE=100;
+	public static int BETPERGAME=1;
+	
+	public void stakeStatus() {
 		int status=(int)(Math.random()*100);
 		if(status<=50)
+		{
 			System.out.println("Win");
+			EVERYDAYSTAKE++;
+		}
+			
 		else
+		{
 			System.out.println("Loose");
+			EVERYDAYSTAKE--;
+		}
+			
+	}
+	public void lossProfit() {
+		while(!(EVERYDAYSTAKE<=50 || EVERYDAYSTAKE>=150)){
+			stakeStatus();
+		}
+		System.out.println("Gambling player has resigned for the day");
+		System.out.println("stake balance:"+ EVERYDAYSTAKE);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("***Welcome to the Gambling Simulator***");
 		GamblingSimulator object=new GamblingSimulator();
-		object.uc1();
-		object.uc2();
+		object.lossProfit();
  
 	}
 
