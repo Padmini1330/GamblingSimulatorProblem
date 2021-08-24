76f84ed17096b095a8077b78f4c28707ca1c5ae9
padmini v <padmini.v@perfios.com>
2021-08-24 19:42:01 +0530

    MODIFIED: Gambling for 20 days to month

--- src/com/bridgelabz/workshop/GamblingSimulator.java
+++ src/com/bridgelabz/workshop/GamblingSimulator.java
@@ -25,23 +25,26 @@ public class GamblingSimulator {
 	}
 	public void gamblingDays()                                 //uc4
 	{
-		int days=20;
+		int days=30;
 		for(int index=1;index<=days;index++)
 		{
 			everyDayStake=100;
 			lossProfit();
 		}
-		System.out.println("total amount: "+ totalAmount);
+		System.out.println("Total stake at the end of "+days+" days "+ totalAmount);
 		
 		
 	}
-	public void lossProfit() {                                  //uc3
-		while(!(everyDayStake<=50 || everyDayStake>=150)){
+	public void lossProfit() 
+	{                                                          //uc3
+		while(!(everyDayStake<=50 || everyDayStake>=150))
+		{
 			stakeStatus();
+			System.out.println("Gambling player has resigned for the day");
+			System.out.println("stake balance:"+ (everyDayStake));
 		}
 		totalAmount+=everyDayStake;
-		System.out.println("Gambling player has resigned for the day");
-		System.out.println("stake balance:"+ everyDayStake);
+		
 	}
 
 	public static void main(String[] args) {
