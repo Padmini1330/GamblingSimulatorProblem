37b86e6ba24fa671f18ece0ae1cb43a2803b9ca2
padmini v <padmini.v@perfios.com>
2021-08-24 08:44:30 +0530

    Added win/loose status

--- src/com/bridgelabz/workshop/GamblingSimulator.java
+++ src/com/bridgelabz/workshop/GamblingSimulator.java
@@ -1,17 +1,25 @@
 package com.bridgelabz.workshop;
 
 public class GamblingSimulator {
-	final static int EVERYDAYSTAKE=100;
-	final static int BETPERGAME=1;
+	public static final int EVERYDAYSTAKE=100;
+	public static final int BETPERGAME=1;
 	public void uc1() {
 		
+	}
+	public void uc2() {
+		int status=(int)(Math.random()*100);
+		if(status<=50)
+			System.out.println("Win");
+		else
+			System.out.println("Loose");
 	}
 
 	public static void main(String[] args) {
 		System.out.println("***Welcome to the Gambling Simulator***");
 		GamblingSimulator object=new GamblingSimulator();
 		object.uc1();
-
+		object.uc2();
+ 
 	}
 
 }
