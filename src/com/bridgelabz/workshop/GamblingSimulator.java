2d15c09f6cdc095f67d51a9867f31a0d4dc0088a
padmini v <padmini.v@perfios.com>
2021-08-24 08:29:37 +0530

    ADDED: constants everyDayStake and everyGameBet

--- src/com/bridgelabz/workshop/GamblingSimulator.java
+++ src/com/bridgelabz/workshop/GamblingSimulator.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.workshop;
 
 public class GamblingSimulator {
+	final static int EVERYDAYSTAKE=100;
+	final static int BETPERGAME=1;
+	public void uc1() {
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("***Welcome to the Gambling Simulator***");
+		GamblingSimulator object=new GamblingSimulator();
+		object.uc1();
 
 	}
 
